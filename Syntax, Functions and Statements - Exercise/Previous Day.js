feab6b6fab7a59bb749f81714dfec16da5e4e81c
Stanislav Stamatov <stanislav.stamatov45@abv.bg>
2022-09-15 22:46:42 +0300

    66/100

--- Syntax, Functions and Statements - Exercise/Previous Day.js
+++
@@ -1,9 +0,0 @@
-function previusDay(year, month, day)
-{
-    let newDate = new Date(year, month - 1, day - 1);
-
-    console.log(`${newDate.getFullYear()}-${newDate.getMonth() + 1}-${newDate.getDate()}`);
-}
-
-previusDay(2016, 9, 30);
-previusDay(2016, 10, 1);
